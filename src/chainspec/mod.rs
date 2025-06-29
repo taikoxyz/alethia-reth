e120516763fb786a7219196a35d700680e3ec5f6
David <david@taiko.xyz>
2025-06-29 09:52:46 +0800

    feat: update `build_evm`

--- src/chainspec/mod.rs
+++ src/chainspec/mod.rs
@@ -1,16 +1,13 @@
 use std::sync::{Arc, LazyLock};
 
 use alloy_chains::{Chain, NamedChain};
-use alloy_genesis::Genesis;
-use alloy_hardforks::{ForkCondition, Hardfork};
-use derive_more::From;
 use reth::{
-    chainspec::{ChainHardforks, ChainSpec, ChainSpecBuilder, make_genesis_header},
+    chainspec::{ChainSpec, make_genesis_header},
     primitives::SealedHeader,
     revm::primitives::{U256, b256},
 };
 
-use crate::chainspec::hardfork::{TAIKO_MAINNET_HARDFORKS, TaikoHardfork};
+use crate::chainspec::hardfork::TAIKO_MAINNET_HARDFORKS;
 
 pub mod hardfork;
 pub mod parser;
@@ -27,7 +24,7 @@ pub static TAIKO_MAINNET: LazyLock<Arc<ChainSpec>> = LazyLock::new(|| {
         chain: Chain::from_named(NamedChain::Taiko),
         genesis_header: SealedHeader::new(
             make_genesis_header(&genesis, &hardforks),
-            b256!("0x7ca38a1916c42007829c55e69d3e9a73265554b586a499015373241b8a3fa48b"),
+            b256!("0x90bc60466882de9637e269e87abab53c9108cf9113188bc4f80bcfcb10e489b9"),
         ),
         genesis,
         paris_block_and_final_difficulty: Some((0, U256::from(0))),
--- src/factory/builder.rs
+++ src/factory/builder.rs
@@ -6,8 +6,10 @@ use reth::{
     builder::{BuilderContext, components::ExecutorBuilder},
     chainspec::ChainSpec,
     providers::BlockReaderIdExt,
+    revm::primitives::{Address, Bytes, ruint::Uint},
 };
 use reth_ethereum::EthPrimitives;
+use tracing::warn;
 
 use crate::{evm::evm::TaikoEvmExtraContext, factory::config::TaikoEvmConfig};
 
@@ -26,29 +28,49 @@ where
         self,
         ctx: &BuilderContext<Node>,
     ) -> impl Future<Output = eyre::Result<Self::EVM>> + Send {
-        let txs = ctx
+        let block = ctx
             .provider()
             .block_by_id(ctx.head().number.into())
             .unwrap()
-            .unwrap()
-            .into_body()
-            .transactions;
+            .unwrap();
 
-        let anchor = txs[0].clone();
+        let txs = block.clone().into_body().transactions;
 
-        let mut nonce = 0;
-        // let mut caller;
-        if let EthereumTxEnvelope::Eip1559(tx) = anchor {
-            nonce = tx.tx().nonce;
+        let mut anchor_caller: Option<Address> = None;
+        let mut anchor_nonce: Option<u64> = None;
+
+        // If the block is not the genesis block, we can extract the anchor transaction.
+        if ctx.head().number != 0 {
+            if txs.len() == 0 {
+                warn!(
+                    "Block {} has no transactions, cannot extract anchor transaction.",
+                    block.header.number
+                );
+                return future::ready(Err(eyre::eyre!(
+                    "Block {} has no transactions, cannot extract anchor transaction.",
+                    block.header.number
+                )));
+            }
+            let anchor = txs[0].clone();
+
+            if let EthereumTxEnvelope::Eip1559(tx) = anchor {
+                anchor_nonce = Some(tx.tx().nonce);
+                anchor_caller = Some(tx.recover_signer().unwrap());
+            }
         }
 
         future::ready(Ok(TaikoEvmConfig::new(
             ctx.chain_spec(),
             TaikoEvmExtraContext::new(
-                50,           // basefee share percentage
-                Option::None, // TODO: implement a way to get the proposer address
-                Some(nonce),
+                decode_ontake_extra_data(block.header.extra_data),
+                anchor_caller,
+                anchor_nonce,
             ),
         )))
     }
+}
+
+fn decode_ontake_extra_data(extradata: Bytes) -> u64 {
+    let value = Uint::<256, 4>::from_be_slice(&extradata);
+    value.as_limbs()[0] as u64
 }

1851dba7653253ec2fa0c555cdcb6e206fea733e
Gavin “yoghurt” Yu <guoyu960223@gmail.com>
2026-04-22 21:10:37 +0800

    fix(primitives): change payload version to 2

--- crates/primitives/src/payload/attributes.rs
+++ crates/primitives/src/payload/attributes.rs
@@ -96,7 +96,7 @@ pub struct TaikoPayloadAttributes {
 impl PayloadAttributes for TaikoPayloadAttributes {
     /// Computes a stable payload identifier for the Taiko payload job.
     fn payload_id(&self, parent_hash: &B256) -> alloy_rpc_types_engine::PayloadId {
-        payload_id_taiko(parent_hash, self, 0)
+        payload_id_taiko(parent_hash, self, 2)
     }
 
     /// Returns the timestamp to be used in the payload job.
